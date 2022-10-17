d646ede6db8e5543599b64a8137351b346e134f2
carotheuri <princesstheush2@gmail.com>
2022-10-17 03:13:29 +0300

    adding a switch case for assigning an akan name

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
 const femaleAkanNames = ['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama'];
 const maleAkanNames = ['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
+var day = document.getElementById("dayborn").value;
+var month = document.getElementById("monthborn").value;
+var year = document.getElementById("yearborn").value;
+var gender = null;
 // const makeAkanNames = [];
-function calculateDayofWeek(day,month,year){
+function calculateDayofWeek(){
     
     console.log("happy");
     var century = year.toString();
     century = century.substring(0,2);
     console.log(century);
     var dayOfWeek =  Math.floor(( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day ) % 7);
-
-    console.log(dayOfWeek);
+    assignUserakanname(dayOfWeek);
 }
 function validateUserDetails(){
     confirm("Are you sure");
-    var day = document.getElementById("dayborn").value;
-    var month = document.getElementById("monthborn").value;
-    var year = document.getElementById("yearborn").value;
-    var gender = null;
+
     if(document.getElementById("female").checked = true){
         gender = "female";
     }
@@ -30,9 +30,39 @@ function validateUserDetails(){
         alert("Kindly provide valid details as suggested on the placeholders");
     }
     else {
-        calculateDayofWeek(day,month,year);
+        calculateDayofWeek();
     }
 }
-function main(){
+function assignUserakanname(calculatedday){
+    if(gender == "female"){
+        switch(calculatedday){
+            case 0:
+                alert(femaleAkanNames[0]);
+            break;
+            case 1:
+                alert(femaleAkanNames[1]);
+            break;
+            case 2:
+                alert(femaleAkanNames[2]);
+            break;
+            case 3:
+                alert(femaleAkanNames[3]);
+            break;
+            case 4:
+                alert(femaleAkanNames[4]);
+            break;
+            case 5:
+                alert(femaleAkanNames[5]);
+            break;
+            case 6:
+                alert(femaleAkanNames[6]);
+            break;
+        }
+    }
     
+    
+    
+}
+function main(){
+    validateUserDetails();
 }
