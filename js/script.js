5fa8cb87b234d3f6ee201b2b4f6aa9e10dcef8c2
carotheuri <princesstheush2@gmail.com>
2022-10-17 05:31:08 +0300

    convert inputs to int

--- js/script.js
+++ js/script.js
@@ -1,32 +1,27 @@
 const femaleAkanNames = ['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama'];
 const testdays = ['monday','tuesday','wednesday','thursday','friday','saturday','sunday'];
 const maleAkanNames = ['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
-
 var gender;
-// const makeAkanNames = [];
 function calculateDayofWeek(year,month,day){
     
-    console.log("happy");
-    var century = year.toString();
-    century = century.substring(0,2);
+
+    year = year.toString();
+    var century = year.substring(0,2);
+    century = parseInt(century);
     year = year.toString();
     var yearlen = year.length;
-    console.log(yearlen);
     var yy = year.substring(yearlen-2,yearlen);
-    console.log(yy);
-    //var dayOfWeek =  Math.round((((century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day) % 7);
-    //console.log(testdays[dayOfWeek]);
-    //assignUserakanname(dayOfWeek);
+    yy = parseInt(yy);
+    var dayOfWeek =  Math.floor((((century/4) -2*century-1) + ((5*yy/4) ) + ((26*(month+1)/10)) + day) % 7);
+    assignUserakanname(dayOfWeek);
 }
 function validateUserDetails(){
     
     confirm("Are you sure");
-    var day = document.getElementById("dayborn").value;
-    var month = document.getElementById("monthborn").value;
-    var year = document.getElementById("yearborn").value;
-    console.log(day);
-    console.log(month);
-    console.log(year);
+    var day = parseInt(document.getElementById("dayborn").value);
+    var month = parseInt(document.getElementById("monthborn").value);
+    var year = parseInt(document.getElementById("yearborn").value);
+   
     if(document.getElementById("female").checked = true){
         gender = "female";
     }
