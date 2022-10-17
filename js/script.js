87c38114dcfe62ee3006414cb780f5ef8af9aa20
carotheuri <princesstheush2@gmail.com>
2022-10-17 04:34:40 +0300

    extracting the yy to be used for extracting the day of the week

--- js/script.js
+++ js/script.js
@@ -1,22 +1,32 @@
 const femaleAkanNames = ['Akosua','Adwoa','Abenaa','Akua','Yaa','Afua','Ama'];
+const testdays = ['monday','tuesday','wednesday','thursday','friday','saturday','sunday'];
 const maleAkanNames = ['Kwasi','Kwadwo','Kwabena','Kwaku','Yaw','Kofi','Kwame'];
-var day = document.getElementById("dayborn").value;
-var month = document.getElementById("monthborn").value;
-var year = document.getElementById("yearborn").value;
-var gender = null;
+
+var gender;
 // const makeAkanNames = [];
-function calculateDayofWeek(){
+function calculateDayofWeek(year,month,day){
     
     console.log("happy");
     var century = year.toString();
     century = century.substring(0,2);
-    console.log(century);
-    var dayOfWeek =  Math.floor(( ( (century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day ) % 7);
-    assignUserakanname(dayOfWeek);
+    year = year.toString();
+    var yearlen = year.length;
+    console.log(yearlen);
+    var yy = year.substring(yearlen-2,yearlen);
+    console.log(yy);
+    //var dayOfWeek =  Math.round((((century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day) % 7);
+    //console.log(testdays[dayOfWeek]);
+    //assignUserakanname(dayOfWeek);
 }
 function validateUserDetails(){
+    
     confirm("Are you sure");
-
+    var day = document.getElementById("dayborn").value;
+    var month = document.getElementById("monthborn").value;
+    var year = document.getElementById("yearborn").value;
+    console.log(day);
+    console.log(month);
+    console.log(year);
     if(document.getElementById("female").checked = true){
         gender = "female";
     }
@@ -30,7 +40,7 @@ function validateUserDetails(){
         alert("Kindly provide valid details as suggested on the placeholders");
     }
     else {
-        calculateDayofWeek();
+        calculateDayofWeek(year,month,day);
     }
 }
 function assignUserakanname(calculatedday){
